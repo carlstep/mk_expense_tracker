d8972b3b5b8d6b5278eddfb2d7e71af9529be221
carlstep <carlstep333@me.com>
2024-08-23 04:56:24 +0700

    expense functionality complete

--- lib/components/my_list_tile.dart
+++ lib/components/my_list_tile.dart
@@ -1,22 +1,49 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
 import 'package:mk_expense_tracker/helper/helper_functions.dart';
 
 class MyListTile extends StatelessWidget {
   final String title;
   final String trailing;
+  final void Function(BuildContext)? onEditPressed;
+  final void Function(BuildContext)? onDeletePressed;
 
   const MyListTile({
     super.key,
     required this.title,
     required this.trailing,
+    required this.onEditPressed,
+    required this.onDeletePressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      trailing: Text(trailing),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          SlidableAction(
+            onPressed: onEditPressed,
+            icon: Icons.settings,
+            backgroundColor: Colors.grey,
+            foregroundColor: Colors.white,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          SlidableAction(
+            onPressed: onDeletePressed,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ],
+      ),
+      child: ListTile(
+        title: Text(title),
+        trailing: Text(trailing),
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mk_expense_tracker/components/my_list_tile.dart';
 import 'package:mk_expense_tracker/database/expense_database.dart';
 import 'package:mk_expense_tracker/helper/helper_functions.dart';
-import 'package:mk_expense_tracker/main.dart';
+
 import 'package:mk_expense_tracker/models/expense.dart';
 import 'package:provider/provider.dart';
 
@@ -24,6 +24,65 @@ class _HomePageState extends State<HomePage> {
     Provider.of<ExpenseDatabase>(context, listen: false).readExpenses();
 
     super.initState();
+  }
+
+  // open delete box
+  void openDeleteBox(Expense expense) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Delete Expense'),
+        // ignore: prefer_const_constructors
+        content: Column(
+          // MainAxisSize.min reduces the height of the column to its children
+          mainAxisSize: MainAxisSize.min,
+        ),
+        actions: [
+          // cancel button
+          _cancelButton(),
+
+          // save button
+          _deleteExpenseButton(expense.id),
+        ],
+      ),
+    );
+  }
+
+  // open edit expense box
+  void openEditBox(Expense expense) {
+    // prefill existing values
+    String existingName = expense.name;
+    String existingAmount = expense.amount.toString();
+
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Edit Expense'),
+        content: Column(
+          // MainAxisSize.min reduces the height of the column to its children
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // user input >> expense name
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(hintText: existingName),
+            ),
+            // user input >> expense amount
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(hintText: existingAmount),
+            ),
+          ],
+        ),
+        actions: [
+          // cancel button
+          _cancelButton(),
+
+          // save button
+          _editExpenseButton(expense),
+        ],
+      ),
+    );
   }
 
   // open new expense box
@@ -76,8 +135,11 @@ class _HomePageState extends State<HomePage> {
               // return a custom list tile
               // MyListTile requires title & trailing
               return MyListTile(
-                  title: eachExpense.name,
-                  trailing: formatAmount(eachExpense.amount));
+                title: eachExpense.name,
+                trailing: formatAmount(eachExpense.amount),
+                onEditPressed: (context) => openEditBox(eachExpense),
+                onDeletePressed: (context) => openDeleteBox(eachExpense),
+              );
             }),
       ),
     );
@@ -129,6 +191,65 @@ class _HomePageState extends State<HomePage> {
         amountController.clear();
       },
       child: const Text('Save'),
+    );
+  }
+
+  // update expense button
+  Widget _editExpenseButton(Expense expense) {
+    return MaterialButton(
+      onPressed: () async {
+        // action for the edit button
+        // only save if one field has been changed
+
+        if (nameController.text.isNotEmpty ||
+            amountController.text.isNotEmpty) {
+          // pop the box
+          Navigator.pop(context);
+
+          // create new updated expense
+          Expense updatedExpense = Expense(
+            name: nameController.text.isNotEmpty
+                ? nameController.text
+                : expense.name,
+
+            // uses helper function to return a double
+            amount: amountController.text.isNotEmpty
+                ? convertStringToDouble(amountController.text)
+                : expense.amount,
+            date: DateTime.now(),
+          );
+
+          // old expense id
+          int exisitngId = expense.id;
+
+          // save to db
+          await context
+              .read<ExpenseDatabase>()
+              .updateExpense(exisitngId, updatedExpense);
+
+          // clear controllers
+          nameController.clear();
+          amountController.clear();
+        }
+
+        nameController.clear();
+        amountController.clear();
+      },
+      child: const Text('Save'),
+    );
+  }
+
+  // delete expense button
+  Widget _deleteExpenseButton(int id) {
+    return MaterialButton(
+      onPressed: () async {
+        // pop the box
+        Navigator.pop(context);
+
+        // delete expense from db
+        await context.read<ExpenseDatabase>().deleteExpense(id);
+      },
+      child: const Text('Delete'),
     );
   }
 }
